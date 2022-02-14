aaa1e7f9b2b984b10751e7ca2c431013ea64971a
MehmetUludag <mehmetuludag96@gmail.com>
2022-02-14 15:29:28 +0300

    first commit

---
+++ arrays/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace arrays
+{
+    
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            // Array Definition
+
+            string[] colours = new string[5];
+            string[] animals = {"Cat", "Dog", "Bird", "Elephant"};
+
+            int[] arr;
+            arr = new int[5];
+
+
+            // Assigning Values to Arrays
+
+            colours[0] = "Blue";
+            arr[3] = 10;
+
+            Console.WriteLine(colours[0]);
+            Console.WriteLine(animals[1]);
+            Console.WriteLine(arr[3]);
+
+
+            // Using Arrays with Loops
+            // Program that calculates the average of n numbers entered from the keyboard
+
+            Console.Write("Please enter the number of elements of the array: ");
+            int arrLength = int.Parse(Console.ReadLine());
+            int[] numArray = new int[arrLength];
+
+            for (int i = 0; i < arrLength; i++)
+            {
+                Console.Write("Please Enter {0}. Number: ", i+1);
+                numArray[i] = int.Parse(Console.ReadLine()); 
+            }
+
+            int total = 0;
+            foreach (var num in numArray)
+            {
+                total += num;
+            }
+
+            Console.WriteLine("Average: " + total/arrLength);
+
+        }
+    }
+}
