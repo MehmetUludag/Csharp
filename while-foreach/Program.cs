73e4ab1cb302793e79a8b3117f5cfeb27687f801
MehmetUludag <mehmetuludag96@gmail.com>
2022-02-14 14:13:06 +0300

    first commit

---
+++ while-foreach/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace while_foreach
+{
+
+    class Program
+    {
+
+    
+        static void Main(string[] args)
+        {
+
+            /* 1'den başlayarak konsola girilen sayıya kadar (sayı dahil) 
+            ortalama hesaplayıp konsola yazdıran program */
+
+            Console.Write("Please Enter A Number:");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while(sayac<=sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+
+            Console.WriteLine(toplam/sayi);
+
+            // a'dan z'ye kadar tüm harfleri konsola yazdıran program
+
+            char character = 'a';
+
+            while(character<='z')
+            {
+                Console.Write(character + " ");
+                character++;  
+            }
+            
+            Console.WriteLine();
+
+            string[] arabalar = {"Ford", "Fiat", "BMW", "Mercedes"};
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+
+        }
+    }
+}
