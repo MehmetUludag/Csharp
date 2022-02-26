bf669bf42c8ab05d33da2e77b933609cb5e4bf45
MehmetUludag <mehmetuludag96@gmail.com>
2022-02-26 19:50:01 +0300

    first commit

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HomeWork
+{
+
+    class HomeWork1
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Lüften Pozitif Bir Sayı Giriniz: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] dizi = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+               dizi[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (var item in dizi)
+            {
+                if(item % 2 == 0)
+                    Console.WriteLine("Girdiğiniz Çift Sayı: " + item);
+            }
+            
+        }
+    }
+}
