68b5bc210f5bac3ce8ddd814ab6d0273de963cb4
MehmetUludag <mehmetuludag96@gmail.com>
2022-03-13 21:33:31 +0300

    Ödev 2-3

---
+++ HomeWork2/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+using System.Collections;
+namespace KoleksiyonlarSoru3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList sesliHarfler = new ArrayList();
+            string cumle = Console.ReadLine();
+            char[] sesli = {'a','e','ı','i','o','ö','u','ü'};
+            for (int i = 0; i < cumle.Length; i++)
+            {
+                if(sesli.Contains(cumle[i]))
+                    sesliHarfler.Add(cumle[i]);
+            }
+
+            foreach (var item in sesliHarfler)
+            {
+                Console.Write(item + " ");
+            }
+
+        }
+    }
+}
