8ef8df204e2529c647df91f0e97aeaca5d30b596
MehmetUludag <mehmetuludag96@gmail.com>
2022-03-13 21:12:30 +0300

    Ödev 2-2

---
+++ HomeWork2/Koleksiyonlar-Soru-2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace KoleksiyonlarSoru2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] enBuyuk = new int[3];
+            int[] enKucuk = new int[3];
+            int[] sayilar = new int[20];
+            int n;
+            Console.Write("Lütfen 20 adet sayı giriniz: ");
+
+            for (int i = 0; i < 20; i++)
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+                sayilar[i] = n;
+            }
+
+            Array.Sort(sayilar);
+            for (int i = 0; i < 3; i++)
+            {
+                enKucuk[i] = sayilar[i];
+                enBuyuk[i] = sayilar[i + 17];
+            }
+
+            Array.Reverse(enBuyuk);
+
+            Console.WriteLine(enBuyuk.Average());
+            Console.WriteLine(enKucuk.Average());
+            Console.WriteLine(enBuyuk.Average() + enKucuk.Average());
+
+        }
+    }
+}
